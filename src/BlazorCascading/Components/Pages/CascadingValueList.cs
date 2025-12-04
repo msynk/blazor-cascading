f89bb32c5b7ac9f7929dbd90630eac3f1eaa25a3
Saleh Yusefnejad <msynk@outlook.com>
2025-12-04 21:24:42 +0330

    remove redundant api

--- src/BlazorCascading/Components/Pages/CascadingValueList.cs
+++ src/BlazorCascading/Components/Pages/CascadingValueList.cs
@@ -3,6 +3,4 @@ namespace BlazorCascading.Components.Pages;
 public class CascadingValueList : List<CascadingValueRecord>
 {
     public new void Add<T>(T value, string? name = null) => base.Add(CascadingValueRecord.From(value, name));
-
-    public void Add(object? value, string? name, Type valueType) => Add(CascadingValueRecord.From(value, name, valueType));
 }
