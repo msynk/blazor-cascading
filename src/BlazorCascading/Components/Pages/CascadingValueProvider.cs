c34a2a5de5556ae454ae7253a723668d535999ff
Saleh Yusefnejad <msynk@outlook.com>
2025-12-04 08:36:33 +0330

    move type to file

--- src/BlazorCascading/Components/Pages/CascadingValueProvider.cs
+++ src/BlazorCascading/Components/Pages/CascadingValueProvider.cs
@@ -3,13 +3,6 @@ using Microsoft.AspNetCore.Components.Rendering;
 
 namespace BlazorCascading.Components.Pages;
 
-public class CascadingValueRecord(string? name, object? value, Type? valuetype = null)
-{
-    public string? Name { get; } = name;
-    public object? Value { get; } = value;
-    public Type ValueType { get; } = valuetype ?? value?.GetType() ?? throw new InvalidOperationException("Value type is missing");
-}
-
 public class CascadingValueProvider : ComponentBase
 {
     [Parameter] public List<CascadingValueRecord> Values { get; set; } = [];
---
+++ src/BlazorCascading/Components/Pages/CascadingValueRecord.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorCascading.Components.Pages;
+
+public class CascadingValueRecord(string? name, object? value, Type? valuetype = null)
+{
+    public string? Name { get; } = name;
+    public object? Value { get; } = value;
+    public Type ValueType { get; } = valuetype ?? value?.GetType() ?? throw new InvalidOperationException("Value type is missing");
+}
